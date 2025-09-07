dc7833440564c7ec3414f5a1fe87e999bd3c9ba2
Asif <asifd@gmail.com>
2025-09-07 18:12:43 +0530

    upgraded wrapAsync file with robust

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,7 +1,17 @@
-function wrapAsync(fn) {
-  return function (req, res, next) {
-    fn(req, res, next).catch(next);
-  };
-}
+// function wrapAsync(fn) {
+//   return function (req, res, next) {
+//     fn(req, res, next).catch(next);
+//   };
+// }
 
-module.exports = wrapAsync;
+// module.exports = wrapAsync;
+
+module.exports = function wrapAsync(fn) {
+  if (typeof fn !== "function") {
+    throw new TypeError(`wrapAsync expected a function but got ${typeof fn}`);
+  }
+  return function (req, res, next) {
+    // Promise.resolve makes it safe for sync or async functions
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
